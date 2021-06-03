68b9f43c7d44ddf7aa44a1351f1f770d2956b1c9
liyajing0314 <571591653@qq.com>
2021-06-03 10:16:30 +0800

    暂存

--- src/api/api.js
+++ src/api/api.js
@@ -4,17 +4,17 @@ import { axios } from '@/utils/request'
  * 
  * @param {Object} params
  */
-// export function getServeData(params) {
-//   return axios({
-//     url: '/servedata/api/getServeData',
-//     method:'get',
-//     param:params
-//   })
-// }
 export function getServeData(params) {
   return axios({
-    url: '/data.json',
+    url: '/servedata/api/getServeData',
     method:'get',
     param:params
   })
 }
+// export function getServeData(params) {
+//   return axios({
+//     url: '/data.json',
+//     method:'get',
+//     param:params
+//   })
+// }
